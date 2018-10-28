e928f9aeaded67055defaa7b839eb677b8163867
nameQyuan <1017850426@qq.com>
2018-10-28 15:40:49 +0800

    轮播效果

---
+++ jclzs/html/js/index.js
@@ -0,0 +1,11 @@
+function aa(){
+    var a1=document.getElementById("a1");
+    a1.src="/images/a1.jpg";
+    document.getElementById("a4").innerHTML=a1;
+}
+
+function bb(){
+    var a1=document.getElementById("a2");
+    a1.src="/images/a2.jpg";
+    document.getElementById("a4").innerHTML=a2;
+}
